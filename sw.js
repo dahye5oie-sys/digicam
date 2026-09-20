da0587596ec05a8542a87111ddc6ab76a04e605c
dahye5oie-sys <dahye5oie@gmail.com>
2026-09-20 17:24:08 +0900

    Add files via upload

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='digicam-v1',F=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(F)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
